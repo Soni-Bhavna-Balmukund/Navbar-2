fd337ef1f6716f18a360dfff660dbea35751f548
bhavna <bsoni7698@email.com>
2024-08-05 17:58:01 +0530

    nav2

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar />
+
+      <Home />
+      
+    </div>
+  );
+}
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import "./HomeStyle.css";
+
+function Home() {
+  return (
+    <>
+      <div className="Home">
+        <div className="rn">React Navbar</div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
